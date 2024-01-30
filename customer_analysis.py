500e5c8a91cbcad561f260b8448dc234ee3ab1d9
hamouzIIpseity <hamzabelea2000@gmail.com>
2024-01-30 10:30:15 +0100

    second commit

---
+++ customer_analysis.py
@@ -0,0 +1,11 @@
+
+import os
+import numpy as np 
+import pandas as pd 
+import warnings
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly.express as px
+
+df = pd.read_csv("C:/Hamza/project kaggle/Customer_personality_analysis/data/marketing_campaign.csv")
+df.head()
